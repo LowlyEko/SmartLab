88b0b510885f7c81b69ebf3820284193897f1324
Adlaonn <lesadlaon01202400392@usep.edu.ph>
2026-05-27 07:58:31 +0800

    Delete SmartLabMay278am2026 directory

--- SmartLabMay278am2026/student/js/utils.js
+++
@@ -1,9 +0,0 @@
-// student/js/utils.js
-
-window.logout = function() {
-  if (confirm("Are you sure you want to logout?")) {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    window.location.href = "../admin/Login-Register.html";
-  }
-};
